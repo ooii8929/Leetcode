ebc4ed945707426f386245cbbcace0f6656e1879
Alvin Lin <alvinlin@kkstream.com>
2024-07-23 22:57:05 +0800

    feat: 1337

--- sort/1337. The K Weakest Rows in a Matrix/main.py
+++ sort/1337. The K Weakest Rows in a Matrix/main.py
@@ -1,33 +1,45 @@
-# class Solution(object):
-#     def kWeakestRows(self, mat, k):
-#         """
-#         :type mat: List[List[int]]
-#         :type k: int
-#         :rtype: List[int]
-#         """
-#         # 遍尋第一個
-#         # 每個都遍尋
-#         record = []
-#         for i in range(len(mat)):
-#             num = 0
-#             for j in range(len(mat[i])):
-#                 if mat[i][j] == 1:
-#                     num += 1
-#             record.append((num,i))
-#         print(record)
-#         sorted_records = sorted(record)
+class Solution(object):
+    def kWeakestRows(self, mat, k):
+        """
+        :type mat: List[List[int]]
+        :type k: int
+        :rtype: List[int]
+        """
+        # 遍尋第一個
+        # 每個都遍尋
+        record = []
+        for i in range(len(mat)):
+            num = 0
+            for j in range(len(mat[i])):
+                if mat[i][j] == 1:
+                    num += 1
+            record.append((num,i))
+        print(record)
+        sorted_records = sorted(record)
 
-#         indices = [record[1] for record in sorted_records]
+        indices = [record[1] for record in sorted_records]
 
-#         return indices[:k]
+        return indices[:k]
 
-list_a = [1,1,1,0]
-list_b = [1,1,0,0]
 
-list_combine = [list_a, list_b]
-print(list_combine)
 
-print(sorted(list_combine))
+# Below is sorted practice
 
-list_sorted = sorted(range(len(list_combine)), key=lambda i: list_combine[i])
-print(list_sorted)
+# list_a = [1,1,1,0]
+# list_b = [1,1,0,0]
+
+# list_combine = [list_a, list_b]
+# print(list_combine)
+
+# print(sorted(list_combine))
+
+# # 數字列表
+# print(sorted([[1, 2], [1, 1], [1, 3]]))
+# # 輸出: [[1, 1], [1, 2], [1, 3]]
+
+# # 字符串列表
+# print(sorted(['apple', 'banana', 'Apple']))
+# # 輸出: ['Apple', 'apple', 'banana']  (大寫字母在小寫字母之前)
+
+# list_sorted = sorted(range(len(list_combine)), key=lambda i: list_combine[i])
+# print(list_sorted)
